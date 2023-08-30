48c566d6c5d18fd4720b6536d056944ca2befea1
MikkelYtting <62931295+MikkelYtting@users.noreply.github.com>
2023-08-30 19:21:44 +0200

    Add project files.

---
+++ Unit Testing Exercises Calculator/CalculatorModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Unit_Testing_Exercises_Calculator
+{
+    public class CalculatorModel
+    {
+        public int Sum(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public int Subtract(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+        public int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        public double Divide(int number1, int number2)
+        {
+            if (number2 == 0)
+            {
+                return 0;
+            }
+            return (double)number1 / number2;
+        }
+    }
+}
